4ffa8774468b6cad166e0b2048d2f659d1a8d271
Alexis Sonolet <alexis.sonolet@gmail.com>
2019-11-22 12:36:57 +0100

    connexion oracle

--- src/javaApp.java
+++ src/javaApp.java
@@ -1,14 +1,18 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class javaApp {
 	public static void main(String[] args) {
 		try {
-			String url = "jdbc:oracle:thin:@ensioracle1.imag.fr:" + "1521:ensioracle1";
+			String url = "jdbc:oracle:thin:@oracle1.imag.fr:" + "1521:oracle1";
 			String user = "sonoleta";
 			String passwd = "asonolet";
 			Connection connection = DriverManager.getConnection(url, user, passwd);
+			
+			Statement stmt = connection.createStatement();
+			stmt.executeQuery(" SELECT * FROM emp");
 		} catch (SQLException e) {
 			e. printStackTrace ();
 		}
