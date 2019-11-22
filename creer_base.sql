53167f2870874f1dd87e4eca34a98d4f98cd41be
Futurne <pstmrtem@outlook.com>
2019-11-22 13:54:44 +0100

    creer_base.sql

---
+++ creer_base.sql
@@ -0,0 +1,78 @@
+--CREATE DATABASE IF NOT EXISTS festival;
+--USE festival;
+
+DROP TABLE planning_artiste;
+DROP TABLE planning_numero;
+DROP TABLE specialite_artiste;
+DROP TABLE pseudo_artiste;
+DROP TABLE evaluation;
+DROP TABLE spectacle;
+DROP TABLE numero;
+DROP TABLE artiste;
+
+CREATE TABLE artiste
+(
+    idArtiste INT PRIMARY KEY NOT NULL,
+    nomArtiste VARCHAR(100),
+    prenomArtiste VARCHAR(100),
+    dateNaissanceArtiste DATE,
+    cirqueArtiste VARCHAR(100)
+);
+
+CREATE TABLE numero
+(
+    idNumero INT PRIMARY KEY NOT NULL,
+    themeNumero VARCHAR(100),
+    resumeNumero VARCHAR(1000),
+    artistePrincipalNumero REFERENCES artiste(idArtiste),
+    dureeNumero INT CHECK(dureeNumero >= 0),
+    noteNumero INT CHECK(noteNumero BETWEEN 0 and 10),
+    evaluationNumero VARCHAR(1000)
+);
+
+CREATE TABLE spectacle
+(
+    dateSpectacle DATE NOT NULL,
+    heureSpectacle INT NOT NULL CHECK (heureSpectacle in (9, 14)),
+    themeSpectacle VARCHAR(100),
+    presentateurSpectacle NOT NULL REFERENCES artiste(idArtiste),
+    prixSpectacle INT NOT NULL CHECK (prixSpectacle >= 0),
+    PRIMARY KEY (dateSpectacle, heureSpectacle)
+);
+
+CREATE TABLE planning_artiste
+(
+    idArtiste INT NOT NULL REFERENCES artiste(idArtiste),
+    idNumero INT NOT NULL REFERENCES numero(idNumero),
+    PRIMARY KEY (idNumero, idArtiste)
+);
+
+CREATE TABLE planning_numero
+(
+    dateSpectacle DATE NOT NULL,
+    heureSpectacle INT NOT NULL,
+    idNumero INT NOT NULL REFERENCES numero(idNumero),
+    FOREIGN KEY (dateSpectacle, heureSpectacle) REFERENCES spectacle(dateSpectacle, heureSpectacle),
+    PRIMARY KEY (dateSpectacle, heureSpectacle)
+);
+
+CREATE TABLE specialite_artiste
+(
+    idArtiste INT PRIMARY KEY NOT NULL REFERENCES artiste(idArtiste),
+    specialiteArtiste VARCHAR(100)
+);
+
+CREATE TABLE pseudo_artiste
+(
+    idArtiste INT PRIMARY KEY NOT NULL REFERENCES artiste(idArtiste),
+    pseudoArtiste VARCHAR(100)
+);
+
+CREATE TABLE evaluation
+(
+    idArtiste INT NOT NULL REFERENCES artiste(idArtiste),
+    idNumero INT NOT NULL REFERENCES numero(idNumero),
+    noteExpert INT NOT NULL CHECK(noteExpert BETWEEN 0 and 10),
+    evaluationExpert VARCHAR(1000),
+    PRIMARY KEY (idArtiste, idNumero)
+);
