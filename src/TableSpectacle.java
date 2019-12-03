ee81aad52ac9ce87b9b83f5f65db80e73de2f90e
ClementMalleret <lamiclem@gmail.com>
2019-12-03 17:18:02 +0100

    premiere version de l'insertion d'un spectacle terminée

--- src/TableSpectacle.java
+++ src/TableSpectacle.java
@@ -10,7 +10,7 @@ public class TableSpectacle extends Table{
 	 * Insert un nouveau spectacle.
 	 * La liste des artistes doit être non-vide.
 	 * Les numeros doivent être rentrés au préalable.
-	 * @param date : date du spectacle
+	 * @param date : date du spectacle sous le format AAAA-MM-JJ ; ex : "2013-09-04"
 	 * @param heure : heure du spectacle
 	 * @param theme : theme du spectacle
 	 * @param presentateur : idArtiste du presentateur du spectacle
@@ -21,7 +21,7 @@ public class TableSpectacle extends Table{
 	 * theme different de celui du spectacle ; l'id du presentateur est invalide ; l'id
 	 * d'au moins 1 des spectacles est invalide
 	 */
-	public void insert(int date, int heure, String theme, int presentateur,
+	public void insert(String date, int heure, String theme, int presentateur,
 			int prix, int[] listeNumeros) throws IllegalArgumentException {
 		//verification des contraintes
 		if (heure != 9 && heure != 14) {
@@ -44,27 +44,51 @@ public class TableSpectacle extends Table{
 				stmt = connection.prepareStatement("SELECT * FROM numero WHERE idNumero = ? ", num);
 				res = stmt.executeQuery();
 				if (!res.next()) {
-					throw new IllegalArgumentException("au moins un idNumero invalide : " + num);
+					throw new IllegalArgumentException("au moins un idNumero invalide dans la liste des numeros"
+							+ " du spectacle : " + num);
 				}
 				stmt = connection.prepareStatement("SELECT theme FROM numero WHERE idNumero = ? ", num);
 				res = stmt.executeQuery();
 				res.next();
 				String themeNum = res.getString(1);
-				if (res.next()) {
+				if (!themeNum.equals(theme)) {
 					throw new IllegalArgumentException("au moins un numero a un theme different de "
-							+ "celui du spectacle. Theme du spectacle : " + theme + " ; theme du "
-							+ "numero : " + themeNum);
+							+ "celui du spectacle. Theme du spectacle : " + theme + " ; id du spectacle"
+									+ " : " + num + "theme du numero : " + themeNum);
 				}
 			}
+			//Toutes les contraintes sont vérifiées : l'insertion est valide
+			//On cree un point de sauvegarde au debut de la transaction
+			
+			//TODO : on enlevera l'autocommit des le debut et faudra virer ca du coup
+			connection.setAutoCommit(false);
 			
 			
+			//creation de la requete créant le spectacle dans la table spectacle
+			stmt = connection.prepareStatement("INSERT INTO spectacle VALUES (?, ?, ?, ?, ?)");
+			stmt.setDate(1, Date.valueOf(date));
+			stmt.setInt(2, heure);
+			stmt.setString(3, theme);
+			stmt.setInt(4, presentateur);
+			stmt.setInt(5, prix);
+			stmt.executeUpdate();
+
+			//creation de la requete pour inserer le planning des numeros
+			for (int numero : listeNumeros) {
+				stmt = connection.prepareStatement("INSERT INTO planning_numero VALUES (?, ?, ?)");
+				stmt.setDate(1, Date.valueOf(date));
+				stmt.setInt(2, heure);
+				stmt.setInt(3, numero);
+				
+				stmt.executeUpdate();
+			}
 			
-			//creation de la requete
+			//fin de la transaction : on commit
+			connection.commit();
+			stmt.close();
 			
-						
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
 	}
 }
