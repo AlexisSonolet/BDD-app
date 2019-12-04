da009500a37d0264c0da6829d06936cd4be4335b
Pierre Pereira <Pierre.Pereira@grenoble-inp.org>
2019-12-04 17:46:21 +0100

    save

--- src/javaApp.java
+++ src/javaApp.java
@@ -63,34 +63,38 @@ public class javaApp {
         menu_str += "4. Retour arrière\n";
 
         int choix = 0;
-        PreparedStatement stmt;
-        while (choix != 4) {
-            System.out.println(menu_str);
-            System.out.print("Votre choix : ");
-            choix = Integer.parseInt(sc.nextLine());
+        PreparedStatement stmt = null;
+        try {
+            while (choix != 4) {
+                System.out.println(menu_str);
+                System.out.print("Votre choix : ");
+                choix = Integer.parseInt(sc.nextLine());
 
-            switch (choix) {
-                case 1: // Artiste
-                    stmt = conn.prepareStatement("SELECT * FROM artiste ORDER BY idArtiste");
-                    break;
-                case 2: // Numero
-                    stmt = conn.prepareStatement("SELECT * FROM numero ORDER BY idNumero");
-                    break;
-                case 3: // Spectacle
-                    stmt = conn.prepareStatement("SELECT * FROM spectacle ORDER BY (dateSpectacle, heureSpectacle)");
-                    break;
-                case 4: // Retour
-                    break;
-                default:
-                    System.out.println("Mauvaise entrée ...\n\n");
-            }
+                switch (choix) {
+                    case 1: // Artiste
+                        stmt = conn.prepareStatement("SELECT * FROM artiste ORDER BY idArtiste");
+                        break;
+                    case 2: // Numero
+                        stmt = conn.prepareStatement("SELECT * FROM numero ORDER BY idNumero");
+                        break;
+                    case 3: // Spectacle
+                        stmt = conn.prepareStatement("SELECT * FROM spectacle ORDER BY (dateSpectacle, heureSpectacle)");
+                        break;
+                    case 4: // Retour
+                        break;
+                    default:
+                        System.out.println("Mauvaise entrée ...\n\n");
+                }
 
-            if (choix >= 1 && choix <= 3) { // On a une query à executer et à afficher
-                ResultSet res = stmt.executeQuery();
-                db.printTable(res);
-                res.close();
-                stmt.close();
+                if (choix >= 1 && choix <= 3) { // On a une query à executer et à afficher
+                    ResultSet res = stmt.executeQuery();
+                    db.printTable(res);
+                    res.close();
+                    stmt.close();
+                }
             }
+        } catch (SQLException e) {
+            e.printStackTrace(System.err);
         }
     }
 }
