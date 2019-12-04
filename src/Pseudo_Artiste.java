378130dc5c6480c6bb65c3e5363e1bd1db0d4779
Pierre Pereira <Pierre.Pereira@grenoble-inp.org>
2019-12-04 17:10:28 +0100

    somethings

--- src/Pseudo_Artiste.java
+++ src/Pseudo_Artiste.java
@@ -19,4 +19,43 @@ public class Pseudo_Artiste extends Table{
 	        e.printStackTrace(System.err);
 	    }
 	}
+
+    public void suppressionPseudo(int idArtiste, String pseudo) {
+        try {
+	        PreparedStatement istm = connection.prepareStatement("DELETE FROM pseudo_artiste WHERE idArtiste = ? AND pseudoArtiste = ?");
+	        istm.setInt(1, idArtiste);
+            istm.setString(2, specialite);
+	        istm.executeQuery();
+	        istm.close();
+        } catch (SQLException e) {
+            e.printStackTrace(System.err);
+        }
+    }
+
+    /**
+     * Look for all artist's pseudos and delete them
+     * from the table.
+     */
+    public void suppAllPseudo(int idArtiste) {
+        try {
+            ArrayList<String> pseudos = new ArrayList<String>();
+
+            // Get the specialities
+            PreparedStatement istm = connection.prepareStatement("SELECT pseudoArtiste FROM pseudo_artiste WHERE idArtiste = ?");
+            istm.setInt(1, idArtiste);
+            ResultSet res = istm.executeQuery();
+
+            while(res.next())
+                pseudos.add(res.getString(1));
+
+            istm.close();
+
+            // Delete them
+            for (String s : pseudos)
+                this.suppressionPseudo(idArtiste, s);
+
+        } catch (SQLException e) {
+            e.printStackTrace(System.err);
+        }
+    }
 }
--- src/Specialite.java
+++ src/Specialite.java
@@ -19,4 +19,43 @@ public class Specialite extends Table {
 	        e.printStackTrace(System.err);
 	    }
 	}
+
+    public void suppressionSpecialite(int idArtiste, String specialite) {
+        try {
+	        PreparedStatement istm = connection.prepareStatement("DELETE FROM specialite_artiste WHERE idArtiste = ? AND specialiteArtiste = ?");
+	        istm.setInt(1, idArtiste);
+            istm.setString(2, specialite);
+	        istm.executeQuery();
+	        istm.close();
+        } catch (SQLException e) {
+            e.printStackTrace(System.err);
+        }
+    }
+
+    /**
+     * Look for all artist' specialities and delete them
+     * from the table.
+     */
+    public void suppAllSpecialite(int idArtiste) {
+        try {
+            ArrayList<String> specs = new ArrayList<String>();
+
+            // Get the specialities
+            PreparedStatement istm = connection.prepareStatement("SELECT specialiteArtiste FROM specialite_artiste WHERE idArtiste = ?");
+            istm.setInt(1, idArtiste);
+            ResultSet res = istm.executeQuery();
+
+            while(res.next())
+                specs.add(res.getString(1));
+
+            istm.close();
+
+            // Delete them
+            for (String s : specs)
+                this.suppressionSpecialite(idArtiste, s);
+
+        } catch (SQLException e) {
+            e.printStackTrace(System.err);
+        }
+    }
 }
