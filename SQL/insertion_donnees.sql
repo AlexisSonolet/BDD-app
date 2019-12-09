ee317f53a68fe9d11425ae2ec67de6e211315c91
Pierre Pereira <Pierre.Pereira@grenoble-inp.org>
2019-12-09 15:57:25 +0100

    plus de donnees SQL

--- SQL/insertion_donnees.sql
+++ SQL/insertion_donnees.sql
@@ -14,6 +14,15 @@ INSERT INTO artiste VALUES (11, 'Linote', 'Kiki', TO_DATE('1990-10-10', 'YYYY-MM
 INSERT INTO artiste VALUES (12, 'Hanounah', 'Cyril', TO_DATE('1991-02-25', 'YYYY-MM-DD'), 'Le grand plateau', '03-18-18-18-18');
 INSERT INTO artiste VALUES (13, 'Lustucru', 'Lapate', TO_DATE('1991-02-25', 'YYYY-MM-DD'), 'Le grand plateau', '03-18-18-18-18');
 INSERT INTO artiste VALUES (14, 'Pinata', 'Lalicorne', TO_DATE('1991-02-25', 'YYYY-MM-DD'), 'Le grand plateau', '03-18-18-18-18');
+INSERT INTO artiste VALUES (15, 'Bonobo', 'Kong', TO_DATE('1901-05-30', 'YYYY-MM-DD'), 'Moderat', '04-28-38-18-18');
+INSERT INTO artiste VALUES (16, '1995', 'Smog', TO_DATE('1971-12-24', 'YYYY-MM-DD'), 'Moderat', '03-18-58-98-18');
+INSERT INTO artiste VALUES (17, 'Tastycool', 'Amsterdam', TO_DATE('1991-02-25', 'YYYY-MM-DD'), 'Im on it', '05-28-88-99-18');
+INSERT INTO artiste VALUES (18, 'Prince', 'Karma', TO_DATE('2001-02-25', 'YYYY-MM-DD'), 'Im on it', '07-28-88-99-18');
+INSERT INTO artiste VALUES (19, 'Indochine', 'Lalune', TO_DATE('1991-03-22', 'YYYY-MM-DD'), 'Im on it', '01-22-88-99-18');
+INSERT INTO artiste VALUES (20, 'Skippy', 'Creamy', TO_DATE('1991-02-25', 'YYYY-MM-DD'), 'Im on it', '07-18-28-99-18');
+INSERT INTO artiste VALUES (21, 'D.A.N.C.E.', 'Juliette', TO_DATE('1990-12-25', 'YYYY-MM-DD'), 'Im on it', '07-18-28-99-18');
+INSERT INTO artiste VALUES (22, 'Tasted', 'Maxence', TO_DATE('1991-02-25', 'YYYY-MM-DD'), 'Piano Delta Piano', '08-28-28-99-18');
+INSERT INTO artiste VALUES (23, 'Cyrin', 'Novo', TO_DATE('1991-02-25', 'YYYY-MM-DD'), 'Piano Delta Piano', '05-18-28-99-18');
 
 -- Table des specialites
 INSERT INTO specialite_artiste VALUES (0, 'Magie');
@@ -33,14 +42,28 @@ INSERT INTO specialite_artiste VALUES (11, 'Voltige');
 INSERT INTO specialite_artiste VALUES (12, 'Zoo');
 INSERT INTO specialite_artiste VALUES (13, 'Clown');
 INSERT INTO specialite_artiste VALUES (14, 'Zoo');
-INSERT INTO specialite_artiste VALUES (14, 'Magie');
+INSERT INTO specialite_artiste VALUES (15, 'Zoo');
+INSERT INTO specialite_artiste VALUES (15, 'Magie');
+INSERT INTO specialite_artiste VALUES (16, 'Voltige');
+INSERT INTO specialite_artiste VALUES (17, 'Voltige');
+INSERT INTO specialite_artiste VALUES (17, 'Clown');
+INSERT INTO specialite_artiste VALUES (18, 'Voltige');
+INSERT INTO specialite_artiste VALUES (19, 'Zoo');
+INSERT INTO specialite_artiste VALUES (20, 'Zoo');
+INSERT INTO specialite_artiste VALUES (21, 'Voltige');
+INSERT INTO specialite_artiste VALUES (21, 'Magie');
+INSERT INTO specialite_artiste VALUES (22, 'Clown');
+INSERT INTO specialite_artiste VALUES (22, 'Voltige');
+INSERT INTO specialite_artiste VALUES (23, 'Zoo');
+INSERT INTO specialite_artiste VALUES (23, 'Clown');
 
 -- Table des numeros
 INSERT INTO numero VALUES (0, 'La machine a sous', 'Magie', 'Ceci est un resume du numero', 10, 30, 8);
 INSERT INTO numero VALUES (1, 'TOP 10 des elephants', 'Zoo', 'Ceci est un resume du numero', 0, 30, 3);
 INSERT INTO numero VALUES (2, 'Une corde cest bien, deux cest mieux', 'Voltige', 'Ceci est un resume du numero', 2, 30, 5);
 INSERT INTO numero VALUES (3, 'Jai un grand sourire', 'Clown', 'Ceci est un resume du numero', 1, 30, 8);
-INSERT INTO numero VALUES (4, 'Imaginez la licorne', 'Zoo', 'Ceci est un resume du numero', 14, 30, 10);
+INSERT INTO numero VALUES (4, 'Imaginez la licorne', 'Zoo', 'Ceci est un resume du numero', 14, 10, 10);
+INSERT INTO numero VALUES (5, 'Quelle vie', 'Clown', 'Ceci est un resume du numero', 17, 20, 7);
 
 -- Table des experts
 INSERT INTO expert VALUES (7);
@@ -49,6 +72,10 @@ INSERT INTO expert VALUES (11);
 INSERT INTO expert VALUES (6);
 INSERT INTO expert VALUES (3);
 INSERT INTO expert VALUES (8);
+INSERT INTO expert VALUES (21);
+INSERT INTO expert VALUES (22);
+INSERT INTO expert VALUES (17);
+INSERT INTO expert VALUES (15);
 
 -- Table des pseudos
 INSERT INTO pseudo_artiste VALUES (14, 'Fornite');
